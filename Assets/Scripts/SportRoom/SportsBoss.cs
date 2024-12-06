67334574004393d84bf1d5b398e497bd98d04f21
Huu-Khoa Nguyen <nguyen.k0801@gmail.com>
2024-12-06 15:48:12 -0800

    Updated Boss Room

--- Assets/Scripts/SportRoom/SportsBoss.cs
+++ Assets/Scripts/SportRoom/SportsBoss.cs
@@ -4,16 +4,40 @@ using UnityEngine;
 
 public class SportsBoss : MonoBehaviour
 {
+    // Variables for the boss
+    public int maxHealth;
+    public int health;
+    public float chargeSpeed;
+    public float chargeCd;
+    public float stunTime;
+
+    private Vector3 chargeDir;
+    private string bossState;
+    private Rigidbody rigidBody;
+    private float timer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch(bossState) 
+        {
+            case "Charging":
+                Charge();
+                break;
+            case "Stunned":
+                Stun();
+                break;
+            case "Cooldown":
+
+                break;
+
+        }
     }
 
     void Stun()
@@ -22,6 +46,11 @@ public class SportsBoss : MonoBehaviour
     }
 
     void Charge()
+    {
+
+    }
+
+    private void OnCollisionEnter() 
     {
 
     }
